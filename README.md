a0aa4a522f320f776fb0cb93c2425dc1ba3da61a
Alan Hanafy <alan.hanafy@gmail.com>
2020-02-25 10:02:31 -0500

    initial

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # kimport
+
+## **Prerequisites**
+### **Install kubectl client**
+
+```
+Download Instructions: https://kubernetes.io/docs/tasks/tools/install-kubectl/
+```
+
+### **Install helm3 client**
+
+```
+Download Instructions: https://github.com/helm/helm/releases
+```
+
+### **Install kubeseal client**
+
+```
+Download Instructions: https://github.com/bitnami-labs/sealed-secrets/releases
+```
+
+` `  
+` `  
+` `  
+
+## **Deploy**
+
+```
+./scripts/flux-init.sh https://github.com/username/reponame.git
+```
+
---
+++ scripts/flux-init.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [[ ! -x "$(command -v kubectl)" ]]; then
+    echo "kubectl not found"
+    echo ">>> download the latest from here: https://kubernetes.io/docs/tasks/tools/install-kubectl/"
+    exit 1
+fi
+
+if [[ ! -x "$(command -v helm)" ]]; then
+    echo "helm not found"
+    echo ">>> download the latest from here: https://github.com/helm/helm/releases"
+    exit 1
+fi
+
+if [[ ! -x "$(command -v kubeseal)" ]]; then
+    echo "kubeseal not found"
+    echo ">>> download the latest from here: https://github.com/bitnami-labs/sealed-secrets/releases"
+    exit 1
+fi
+
+REPO_ROOT=$(git rev-parse --show-toplevel)
+REPO_URL=${1:-https://github.com/ahanafy/cluster-base.git}
+REPO_BRANCH='master'
+REPO_PUBLIC=true
+
+echo ">>> Creating Flux Namespace"
+kubectl apply -f https://raw.githubusercontent.com/ahanafy/cluster-base/master/base/fluxcd/namespace.yaml
+
+helm repo add fluxcd https://charts.fluxcd.io
+
+echo ">>> Installing Flux for ${REPO_URL}"
+helm upgrade -i fluxcd fluxcd/flux --wait --cleanup-on-fail \
+--set git.url=${REPO_URL} \
+--set git.branch=${REPO_BRANCH} \
+--set git.pollInterval=1m \
+--set git.readonly=${REPO_PUBLIC} \
+--set registry.pollInterval=1m \
+--namespace fluxcd
+
+echo ">>> Installing Helm Operator"
+helm upgrade -i helm-operator fluxcd/helm-operator --wait --cleanup-on-fail \
+--set git.ssh.secretName=fluxcd-git-deploy \
+--set helm.versions=v3 \
+-f https://raw.githubusercontent.com/ahanafy/cluster-base/master/base/fluxcd/repositories-configmap.yaml \
+--namespace fluxcd
+
+echo '>>> GitHub deploy key'
+kubectl -n fluxcd logs deployment/fluxcd | grep identity.pub | cut -d '"' -f2
